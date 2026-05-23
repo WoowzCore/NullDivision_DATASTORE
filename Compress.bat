bc29b787ee397bd3f433487ec14376f7dc6523f4
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-05-23 22:09:55 +0300

    version.txt

--- Compress.bat
+++ Compress.bat
@@ -3,11 +3,21 @@ setlocal
 
 set "SourceFolder=DATA"
 set "ZipFile=DATA.zip"
+set "VersionFile=Version.txt"
 
 if exist "%ZipFile%" del "%ZipFile%"
+
+if exist "%VersionFile%" (
+	for /f "tokens=*" %%a in ('type "%VersionFile%"') do set "Version=%%a"
+	set /a "Version+=1"
+) else (
+	set "Version=1"
+)
+<nul set /p "=%Version%" > "%VersionFile%"
+echo Version updated to: %Version%
 
 pushd "%SourceFolder%"
 7z a -tzip "..\%ZipFile%" * -mx=1 -mmt=on -r -bb0 >nul
 popd
 
-echo Done!
+echo Done! Version: %Version%
