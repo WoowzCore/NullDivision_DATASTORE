11b3a7f16eeb633e5b17a8fee847725d2af1cf0e
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-05-25 18:54:14 +0300

    TO PACK

--- DATA/script/sandbox.js
+++ DATA/script/sandbox.js
@@ -7,6 +7,6 @@ _G.SetEvent(_G.Enum.Event.ClientTick, function(End){
     //_G.ApplyAnomaly(_G.Enum.AnomalyType.SunSize, i/10)
 })
 
-//_G.ApplyAnomaly(_G.Enum.Anomaly.SkyRender, _G.Enum.SkyRender.End)
-//_G.ApplyAnomaly(_G.Enum.Anomaly.SkyColor, 1, 1, 1)
-//_G.ApplyAnomaly(_G.Enum.Anomaly.SkyEndTexture, _G.ResourceLocation(_G.Constant.TempID, "test.png"))
+_G.ApplyAnomaly(_G.Enum.Anomaly.SkyRender, _G.Enum.SkyRender.End)
+_G.ApplyAnomaly(_G.Enum.Anomaly.SkyColor, 1, 1, 1)
+_G.ApplyAnomaly(_G.Enum.Anomaly.SkyEndTexture, _G.ResourceLocation(_G.Constant.TempID, "textures/test.png"))
