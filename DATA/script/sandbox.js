45d3e0a33a7cda6f38b2bc76ed003c75dd195527
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-05-25 19:49:18 +0300

    savewevwef

--- DATA/script/sandbox.js
+++ DATA/script/sandbox.js
@@ -4,10 +4,9 @@ _G.SetEvent(_G.Enum.Event.ClientTick, function(End){
 
     i++
 
-    //_G.ApplyAnomaly(_G.Enum.AnomalyType.SunSize, i/10)
+    _G.ApplyAnomaly(_G.Enum.Anomaly.SkyColor, _G.DSin(i/15), _G.DSin(i/10), _G.DSin(i/20))
 })
 
 _G.ApplyAnomaly(_G.Enum.Anomaly.SkyRender, _G.Enum.SkyRender.End)
-_G.ApplyAnomaly(_G.Enum.Anomaly.SkyColor, 1, 0, 0)
 _G.ApplyAnomaly(_G.Enum.Anomaly.SkyEndTiles, 10)
 _G.ApplyAnomaly(_G.Enum.Anomaly.SkyEndTexture, _G.ResourceLocation("minecraft", "textures/block/dirt.png"))
