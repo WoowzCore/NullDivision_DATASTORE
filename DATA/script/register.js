669327071608757375ee99da5653ed9f370e3680
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-05-26 21:35:39 +0300

    REGISTER ITEM BLOCK

--- DATA/script/register.js
+++ DATA/script/register.js
@@ -1,9 +1,34 @@
+var addedblocks = []
+
 WL.SetEvent(WL.Enum.Event.Register, function(RegisterType){
+    if(RegisterType == WL.Enum.Register.Block){
+        for(var i = 0; i < 5; i++){
+            var id = "block_example_" + i;
+            var Info = new WL.BuiltinBlock(id)
+
+            Info.Name = "golu " + i
+
+            addedblocks.push(id)
+
+            WL.Register(Info)
+        }
+    }
+
     if(RegisterType == WL.Enum.Register.Item){
-        for(var i = 0; i < 100; i++){
+        for(var i = 0; i < 5; i++){
             var Info = new WL.BuiltinItem("example_" + i)
 
             Info.Name = "golu " + i
+
+            WL.Register(Info)
+        }
+
+        for(var j = 0; j < addedblocks.length; j++){
+            var blockid = addedblocks[j]
+
+            var Info = new WL.BuiltinItem("block_" + blockid)
+
+            Info.Block = WL.Constant.ID + ":" + blockid
 
             WL.Register(Info)
         }
